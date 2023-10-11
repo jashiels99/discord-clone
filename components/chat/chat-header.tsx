d61257493b93dcf92de09d8540793a89d98f2dce
Joe Shiels <joeshiels@Joes-MacBook-Pro.local>
2023-10-11 19:42:51 +0100

    FEAT: Basic conversations setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from 'lucide-react';
 import MobileToggle from '@/components/mobile-toggle';
+import UserAvatar from '../user-avatar';
 
 interface ChatHeaderProps {
     serverId: string;
@@ -19,6 +20,12 @@ export default function ChatHeader({
             <MobileToggle serverId={serverId} />
             {type === 'channel' && (
                 <Hash className="w-5 h-5 mr-2 text-zinc-500 dark:text-zinc-400" />
+            )}
+            {type === 'conversation' && (
+                <UserAvatar
+                    src={imageUrl}
+                    className="mr-2 w-7 h-7 md:h-7 md:w-7"
+                />
             )}
             <p className="font-semibold text-black text-md dark:text-white">
                 {name}
